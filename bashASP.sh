20d890384fd5f642f174a1235ddd0da7635ec0a7
boukrisw <45873715+boukrisw@users.noreply.github.com>
2021-04-29 19:59:58 +0200

    Tests

--- bashASP.sh
+++ bashASP.sh
@@ -1,32 +1,30 @@
 javac -d classes -cp classes:lib/pddl4j-3.8.3.jar:lib/sat4j-sat.jar src/fr/uga/pddl4j/tutorial/asp/ASP.java -Xlint:unchecked
 
 
-#########  blocksworld
+#########  blocksworld   DONE
 
-#for i in `seq 1 9`;
-#do
-#  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/blocksworld/domain.pddl -f pddl/exemples/blocksworld/"p0"$i".pddl" -t 300
-#done
+for i in `seq 1 9`;
+do
+  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/blocksworld/domain.pddl -f pddl/exemples/blocksworld/"p0"$i".pddl" -t 300
+done
 
 
-#for i in `seq 10 15`;
-#do
-#  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/blocksworld/domain.pddl -f pddl/exemples/blocksworld/"p"$i".pddl" -t 300
-#done
+for i in `seq 10 15`;
+do
+  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/blocksworld/domain.pddl -f pddl/exemples/blocksworld/"p"$i".pddl" -t 300
+done
 
 
 #########  depots
 
-#for i in `seq 1 9`;
-#do
-#  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/depots/domain.pddl -f pddl/exemples/depots/"p0"$i".pddl" -t 300
-#done
+for i in `seq 5 9`;
+do
+  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/depots/domain.pddl -f pddl/exemples/depots/"p0"$i".pddl" -t 300
+done
 
 
-#for i in `seq 10 15`;
-#do
-#  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/depots/domain.pddl -f pddl/exemples/depots/"p"$i".pddl" -t 300
-#done
+java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/depots/domain.pddl -f pddl/exemples/depots/p10.pddl -t 300
+
 
 #########  gripper
 
@@ -35,11 +33,8 @@ do
   java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/gripper/domain.pddl -f pddl/exemples/gripper/"p0"$i".pddl" -t 300
 done
 
+java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/gripper/domain.pddl -f pddl/exemples/gripper/p10.pddl -t 300
 
-for i in `seq 10 15`;
-do
-  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/gripper/domain.pddl -f pddl/exemples/gripper/"p"$i".pddl" -t 300
-done
 
 #########  logistics
 
@@ -48,8 +43,4 @@ do
   java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/logistics/domain.pddl -f pddl/exemples/logistics/"p0"$i".pddl" -t 300
 done
 
-
-for i in `seq 10 15`;
-do
-  java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/logistics/domain.pddl -f pddl/exemples/logistics/"p"$i".pddl" -t 300
-done
+java -cp classes:lib/pddl4j-3.8.3.jar fr.uga.pddl4j.tutorial.asp.ASP -o pddl/exemples/logistics/domain.pddl -f pddl/exemples/logistics/p10.pddl -t 300
