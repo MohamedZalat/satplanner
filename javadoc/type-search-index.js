3273f1aed26bf93c8e3e0a71f87f0ec56a017828
Gauthier <flogauth@outlook.fr>
2021-04-30 20:13:33 +0200

    Create javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"fr.uga.pddl4j.tutorial.satplanner","l":"SATEncoding"},{"p":"fr.uga.pddl4j.tutorial.satplanner","l":"SATPlanner"}];updateSearchResults();
